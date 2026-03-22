efec6473e6fa616829fdd8963db984ae354dccc3
natale565 <natale565@icloud.com>
2026-03-22 19:38:47 -0400

    clock.cpp file created

---
+++ clock.cpp
@@ -0,0 +1,19 @@
+#ifndef CLOCK_H_INCLUDED
+#define CLOCK_H_INCLUDED
+#include <string>
+
+void setTime(int, int, int);
+
+std::string twoDigitString(int);
+std::string nCharString(size_t n, char c);
+std::string formatTime24(unsigned int h, unsigned int m, unsigned int s);
+std::string formatTime12(unsigned int h, unsigned int m, unsigned int s);
+void printMenu(char *strings[], unsigned int numStrings, unsigned char width);
+unsigned int getMenuChoice(unsigned int maxChoice);
+void displayClocks(unsigned int h, unsigned int m, unsigned int s);
+void mainMenu();
+void addOneHour();
+void addOneMinute();
+void addOneSecond();
+
+#endif
